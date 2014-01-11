afa686dc80b198c8db9f43b9b0e6b696fc398904
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2014-01-11 13:04:15 -0300

    Moving to JSON storage

---
+++ js/app.js
@@ -0,0 +1,146 @@
+var starloggerApp = angular.module('starloggerApp', ['ngStorage', 'ngRoute'])
+
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'planetListCtrl',
+      templateUrl: 'planetList.html'
+    })
+    
+    .when('/details/:planetName', {
+      controller: 'planetDetailsCtrl',
+      templateUrl: 'planetDetail.html'
+    })
+
+    .when('/edit/:planetName', {
+      controller: 'planetEditCtrl',
+      templateUrl: 'planetForm.html'
+    })
+
+    .when('/add-planet', {
+      controller: 'planetAddCtrl',
+      templateUrl: 'planetForm.html'
+    })
+
+    .otherwise({
+      redirectTo: '/'
+    });
+})
+
+.controller('planetListCtrl', function($scope, $localStorage, $http) {
+
+  var fs = require('fs');
+  // copy local storage to the $storage service.
+  $scope.$storage = $localStorage;
+  fs.readFile("planets.json", function(err, data) {
+    if(err) {
+      alert("ERROR");
+    }
+    console.log(JSON.parse(data));
+    $scope.planets = JSON.parse(data);
+  });
+
+  // Check if the storage.planetList is empty or unexistant.
+  // If so, make it an empty array.
+  
+  if ($scope.$storage.planetList == 0 || $scope.$storage.planetList == undefined) {
+    console.debug("planetList should be empty");
+    $scope.planets = $scope.$storage.planetList = {};
+  }
+  // If not, assign it to the planets variable.
+  else {
+    console.debug("planetList seems to already have content");
+    $scope.planets = $scope.$storage.planetList
+  }
+  
+
+  $scope.deleteAll = function() {
+    $localStorage.$reset();
+    $scope.$storage = $localStorage;
+    $scope.planets = {};
+  }
+
+  // Save the modifications donde to planets into the storage.
+  //$scope.$storage.planetList = $scope.planets;
+  var jfile = JSON.stringify($scope.planets);
+
+  fs.writeFile("planets.json", jfile, function(err) {
+    if(err) {
+      alert("error");
+    }
+  });
+})
+
+.controller('planetDetailsCtrl', function($scope, $localStorage, $routeParams, $location) {
+  $scope.$storage = $localStorage;
+  $scope.planet = $scope.$storage.planetList[$routeParams.planetName];
+
+  $scope.deletePlanet = function() {
+    var planet = $scope.planet;
+    console.debug("deletePlanet("+planet.name+")");
+    delete $scope.$storage.planetList[$routeParams.planetName];
+    $location.path('/');
+  }
+})
+
+.controller('planetEditCtrl', function($scope, $localStorage, $routeParams, $location) {
+  $scope.$storage = $localStorage;
+  var planet = $scope.planet = $scope.$storage.planetList[$routeParams.planetName];
+  $scope.newPlanet = planet;
+
+  $scope.save = function() {
+    $scope.newPlanet.sector =
+      $scope.newPlanet.name.split(" ")[0].toLowerCase();
+    $scope.planet = $scope.newPlanet;
+
+    $scope.$storage.planetList[$scope.planet.name] = $scope.planet;
+    delete $scope.$storage.planetList[$routeParams.planetName];
+    $location.path('/details/'+$scope.planet.name);
+  }
+})
+
+.controller('planetAddCtrl', function($scope, $localStorage, $location) {
+  $scope.$storage = $localStorage;
+
+  $scope.save = function() {
+    $scope.newPlanet.sector =
+      $scope.newPlanet.name.split(" ")[0].toLowerCase();
+    $scope.$storage.planetList[$scope.newPlanet.name] = $scope.newPlanet;
+
+    $location.path('/');
+  }
+
+})
+
+.directive('slPlanets', function() {
+  return {
+    replace: true,
+    restrict: 'E',
+    templateUrl: 'templates/planets.html'
+  };
+})
+
+.directive('slPlanet', function() {
+  return {
+    transclude: true,
+    restrict: 'E',
+    templateUrl: 'templates/planet.html'
+  };
+})
+
+.directive('slTitlebar', function() {
+  return {
+    replace: true,
+    restrict: 'E',
+    template: '<div id="titlebar"><a href="#/" id="title">Starlogger</a> <div onClick="window.close()" id="close">x</div></div>'
+  };
+})
+
+
+.directive('slSidebar', function() {
+  return {
+    replace: true,
+    restrict: 'E',
+    templateUrl: 'templates/sidebar.html'
+  };
+});
